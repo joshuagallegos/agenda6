df3393d7468c1dfb509ed5e1e6f63725d8d62939
joshuagallegos <joshualeviantonio@gmail.com>
2019-03-19 22:13:31 -0600

    Pruebas de interfaz

---
+++ UCA.WebCalculator/src/Benday.WebCalculator.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UCA.WebCalculator.WebUi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UCA.WebCalculator/src/Benday.WebCalculator.WebUi/Models/ICalculatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace UCA.WebCalculator.Tests.Presentation
+{
+    public interface ICalculatorViewModel
+    {
+        bool IsResultValid { get; set; }
+        string Message { get; set; }
+        string Operator { get; set; }
+        List<SelectListItem> Operators { get; set; }
+        double ResultValue { get; set; }
+        double Value1 { get; set; }
+        double Value2 { get; set; }
+    }
+}
